1ddd234d0c9c2f2e9de6cbcc28ceb483e9efaa94
Elijah <bundielly@gmail.com>
2022-06-13 17:40:34 +0300

    ToDoList done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target['new-task-description'].value)  
+    form.reset()
+  })
 });
+
+function handleToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X';
+  li.textContent = `${todo}   `;
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+// document.createElement('li')
+
+// console.log(document.querySelector('#tasks'))
+
+
+
+
+
+
+
+
+
+
